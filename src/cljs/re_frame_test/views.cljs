1a374b79acb48f9e2a3c10e11260d6a90b4a2d93
Brjánn Ljótsson <brjann@gmail.com>
2020-12-28 20:49:06 +0100

    Update re-com, add demo of failing test, remove iml file from repo

--- src/cljs/re_frame_test/views.cljs
+++ src/cljs/re_frame_test/views.cljs
@@ -1,16 +1,26 @@
 (ns re-frame-test.views
   (:require
-   [re-frame.core :as re-frame]
-   [re-com.core :as re-com]
-   [re-frame-test.subs :as subs]))
+    [re-frame.core :as re-frame]
+    [re-com.core :as re-com]
+    [re-frame-test.subs :as subs]))
 
-(re-frame/reg-sub :test-sub
+(re-frame/reg-sub
+  :test-sub
   (fn [db]
     (:test-sub db)))
 
-(re-frame/reg-event-db :test-event
+(re-frame/reg-sub
+  :test-result
+  (fn [db]
+    (:test-result db)))
+
+(re-frame/reg-event-db
+  :test-event
   (fn [db [_ value]]
-    (assoc db :test-sub value)))
+    (if (= 1 (first (shuffle [1 2])))
+      (assoc db :test-sub value
+                :test-result "Change accepted")
+      (assoc db :test-result "Change ignored"))))
 
 (defn title []
   (let [name (re-frame/subscribe [::subs/name])]
@@ -19,11 +29,13 @@
      :level :level1]))
 
 (defn main-panel []
-  (let [test (re-frame/subscribe [:test-sub])]
+  (let [test   (re-frame/subscribe [:test-sub])
+        result (re-frame/subscribe [:test-result])]
     [re-com/v-box
      :height "100%"
      :children [[title]
                 [:span @test]
+                [:span @result]
                 [re-com/input-text
                  :model test
                  :on-change #(re-frame/dispatch [:test-event %])]]]))
