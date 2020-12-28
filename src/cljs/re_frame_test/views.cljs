73efbe60d01bb086b0a4c70d6704bafbc385ab4b
Brjánn Ljótsson <brjann@gmail.com>
2020-12-28 21:57:22 +0100

    Add value modification

--- src/cljs/re_frame_test/views.cljs
+++ src/cljs/re_frame_test/views.cljs
@@ -17,10 +17,12 @@
 (re-frame/reg-event-db
   :test-event
   (fn [db [_ value]]
-    (if (= 1 (first (shuffle [1 2])))
-      (assoc db :test-sub value
-                :test-result "Change accepted")
-      (assoc db :test-result "Change ignored"))))
+    (case (first (shuffle [1 2 3]))
+      1 (assoc db :test-sub value
+                  :test-result "Change accepted")
+      2 (assoc db :test-result "Change ignored")
+      3 (assoc db :test-sub (subs value 1)
+                  :test-result "First letter removed"))))
 
 (defn title []
   (let [name (re-frame/subscribe [::subs/name])]
@@ -34,8 +36,8 @@
     [re-com/v-box
      :height "100%"
      :children [[title]
-                [:span @test]
-                [:span @result]
+                [:span (str "DB-value: " @test)]
+                [:span (str "Last change result: " @result)]
                 [re-com/input-text
                  :model test
                  :on-change #(re-frame/dispatch [:test-event %])]]]))
